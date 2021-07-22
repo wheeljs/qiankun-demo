6b7bf192782b17e35456573733524736725caacf
wheeljs <ttvip88888888@foxmail.com>
2021-07-22 14:45:21 +0800

    feat(main): runnable

---
+++ main-app/build/webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    devtool: 'source-map',
+    devServer: {
+        host: '0.0.0.0',
+        port: '7099',
+        clientLogLevel: 'warning',
+        disableHostCheck: true,
+        compress: true,
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ]
+};
---
+++ main-app/src/index.js
@@ -0,0 +1,18 @@
+import { registerMicroApps, start } from 'qiankun';
+
+registerMicroApps([
+    {
+        name: 'purehtml',
+        entry: '//localhost:8081/about.html',
+        container: '#subapp-container',
+        activeRule: '/about',
+    },
+    {
+        name: 'admin-app',
+        entry: '//localhost:8080/sub.html',
+        container: '#subapp-container',
+        activeRule: '/admin',
+    }
+]);
+
+start();
